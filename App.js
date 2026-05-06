fda25857996d220f161afe506e8f4753b065fbc1
LUIZ HENRIQUE ANDRADE SANTOS <andradesantos132019@gmail.com>
2026-05-06 15:53:41 -0300

    a

--- App.js
+++ App.js
@@ -3,32 +3,36 @@ import { SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import { styles } from './src/styles/index.js';
-import { routes } from './src/routes';
-import useSimpleNavigation from './src/navigation/useSimpleNavigation';
+import { routes } from './src/routes.js';
+import useSimpleNavigation from './src/navigation/useSimpleNavigation.js';
 
-import Home from './src/telas/Home';
-import Sobre from './src/telas/Sobre';
-import Funcionalidades from './src/telas/Funcionalidades';
-import Contato from './src/telas/Contato';
-import Login from './src/telas/Login';
-import Esqueci from './src/telas/Esqueci';
-import Laudos from './src/telas/Laudos';
-import Notificacoes from './src/telas/Notificacoes';
-import MenuPaciente from './src/telas/MenuPaciente';
-import PreencherLaudos from './src/telas/PreencherLaudos';
-import Favoritos from './src/telas/Favoritos';
-import BuscarCid from './src/telas/BuscarCid';
-import BuscarProcedimentos from './src/telas/BuscarProcedimentos';
-import Configuracoes from './src/telas/Configuracoes';
-import Perfil from './src/telas/Perfil';
-import Seguranca from './src/telas/Seguranca';
-import Suporte from './src/telas/Suporte';
-import Termos from './src/telas/Termos';
-import AlterarEmail from './src/telas/AlterarEmail';
-import AlterarSenha from './src/telas/AlterarSenha';
-import FaleConosco from './src/telas/FaleConosco';
-import Horarios from './src/telas/Horarios';
-import AdicionarFavoritos from './src/telas/AdicionarFavoritos';
+import Home from './src/telas/Home/index.js';
+import Sobre from './src/telas/Sobre/index.js';
+import Funcionalidades from './src/telas/Funcionalidades/index.js';
+import Contato from './src/telas/Contato/index.js';
+import Login from './src/telas/Login/index.js';
+import Esqueci from './src/telas/Esqueci/index.js';
+
+import Laudos from './src/telas/Laudos/index.js';
+import Notificacoes from './src/telas/Notificacoes/index.js';
+import MenuPaciente from './src/telas/MenuPaciente/index.js';
+import PreencherLaudos from './src/telas/PreencherLaudos/index.js';
+import Favoritos from './src/telas/Favoritos/index.js';
+
+import BuscarCid from './src/telas/BuscarCid/index.js';
+import BuscarProcedimentos from './src/telas/BuscarProcedimentos/index.js';
+
+import Configuracoes from './src/telas/Configuracoes/index.js';
+import Perfil from './src/telas/Perfil/index.js';
+import Seguranca from './src/telas/Seguranca/index.js';
+import Suporte from './src/telas/Suporte/index.js';
+import Termos from './src/telas/Termos/index.js';
+
+import AlterarEmail from './src/telas/AlterarEmail/index.js';
+import AlterarSenha from './src/telas/AlterarSenha/index.js';
+import FaleConosco from './src/telas/FaleConosco/index.js';
+import Horarios from './src/telas/Horarios/index.js';
+import AdicionarFavoritos from './src/telas/AdicionarFavoritos/index.js';
 
 export default function App() {
   const nav = useSimpleNavigation();
@@ -55,21 +59,23 @@ export default function App() {
       [routes.favoritos]: Favoritos,
       [routes.buscarCid]: BuscarCid,
       [routes.buscarProcedimentos]: BuscarProcedimentos,
+
       [routes.configuracoes]: Configuracoes,
       [routes.perfil]: Perfil,
       [routes.seguranca]: Seguranca,
+      [routes.suporte]: Suporte,
+      [routes.termos]: Termos,
+
       [routes.alterarEmail]: AlterarEmail,
       [routes.alterarSenha]: AlterarSenha,
-      [routes.suporte]: Suporte,
       [routes.faleConosco]: FaleConosco,
       [routes.horarios]: Horarios,
-      [routes.termos]: Termos,
       [routes.adicionarFavoritos]: AdicionarFavoritos,
     }),
     []
   );
 
-  const Screen = screens[screen] || Home;
+  const Screen = screens[screen] || Login;
 
   return (
     <SafeAreaView style={styles.app}>
